84245b00870436092fe398533d8a5b922a555423
sakithmademel <140998645+sakithmademel@users.noreply.github.com>
2023-07-31 09:58:29 +0530

    Add files via upload

---
+++ Lab04-Q1(1)/Lab04-Q1(1)/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab04_Q1_1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConvertValues obj = new ConvertValues();
+
+            obj.kilometerToMeter();
+        }
+    }
+    class ConvertValues
+    {
+        public void kilometerToMeter()
+        {
+
+            double meter = 1000;
+
+            Console.WriteLine("1 kilometer = " +meter);
+        }
+    }
+}
