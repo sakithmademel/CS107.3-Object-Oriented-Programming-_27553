09525fdbacb1c85a1a7c1bb5c4c6099374df5805
sakithmademel <140998645+sakithmademel@users.noreply.github.com>
2023-07-31 10:03:48 +0530

    Add files via upload

---
+++ Q3#/Q3#/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Q3_
+{
+    public partial class Form1 : Form
+    {
+        int num=1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = num.ToString();
+            num++;
+
+
+        }
+    }
+}
---
+++ q1#/q1#/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace q1_
+{
+    public partial class Form1 : Form
+    {   
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num = 1;
+            label1. Text= num.ToString();
+             
+        }
+    }
+}
